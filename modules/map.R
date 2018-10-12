fecb14056411138a40e1138180244a9a597ae55e
Dan Fornika <dfornika@gmail.com>
2018-10-12 16:16:41 -0700

    Add popups to map markers.

--- modules/map.R
+++ modules/map.R
@@ -11,14 +11,19 @@ map_server = function(input,output,session, query_table){
         
         
         df = query_table()
-        samples = df %>% select(SAMPLE, LAT, LONG)  %>% unique
+        samples = df %>% select(SAMPLE, LAT, LONG, DEPTHM)  %>% unique
         
         
         m <- leaflet(data = samples) %>%
             addTiles() %>%
-            setView(lng=-73.935242, lat=40.730610 , zoom=10) %>% 
+            
+            setView(lng=12.491348, lat=41.902281 , zoom=4) %>% 
             addMarkers(lng = ~LONG,
-                       lat = ~LAT)
+                       lat = ~LAT,
+                       popup = paste("sample: ", samples$SAMPLE, "<br/>",
+                                     "latitude: ", samples$LAT, "<br/>",
+                                     "longitude: ", samples$LONG, "<br/>",
+                                     "depth (m): ", samples$DEPTHM))
             
         m
     })
