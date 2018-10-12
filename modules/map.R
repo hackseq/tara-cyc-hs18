192615b145e125976069cb87ce3f23c814d06054
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-10-12 13:06:46 -0700

    points

--- modules/map.R
+++ modules/map.R
@@ -7,9 +7,19 @@ map_ui = function(id){
 
 map_server = function(input,output,session, query_table){
     output$map <- renderLeaflet({
-        m <- leaflet() %>%
+        print(head(query_table()))
+        
+        
+        df = query_table()
+        samples = df %>% select(SAMPLE, LAT, LONG)  %>% unique
+        
+        
+        m <- leaflet(data = samples) %>%
             addTiles() %>%
-            setView(lng=-73.935242, lat=40.730610 , zoom=10)
+            setView(lng=-73.935242, lat=40.730610 , zoom=10) %>% 
+            addMarkers(lng = ~LONG,
+                       lat = ~LAT)
+            
         m
     })
 }
