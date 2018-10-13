19e341d16faab9d7ba601b601a8d7a3322361b25
klgray25 <kristen.leanne252@gmail.com>
2018-10-13 16:36:58 -0700

    Saving changes

---
+++ modules/table.R
@@ -0,0 +1,8 @@
+table_ui = function(id){
+    ns = NS(id)
+    tagList()
+}
+
+table_server = function(input,output,session, query_table){
+    
+}
---
+++ modules/welcome.R
@@ -0,0 +1,6 @@
+welcome_ui = function(){
+    tags$div(class = "welcomebox",
+             tags$h1('Tara Ocean Virome Hallam Lab'),
+             tags$h1('Metapathways Tool')
+             )
+}
