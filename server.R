cc3d560e5629449c613a702375e62b5908eb62e2
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-10-13 16:39:34 -0700

    massive map modification

--- server.R
+++ server.R
@@ -1,22 +1,12 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
+shinyServer(function(input, output,session) {
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-    
     query_table = reactiveVal(tara_data)
-    
-    callModule(query_server,'query',query_table)
-    
-    mapChoice = callModule(map_server,'map', query_table)
-    
-    callModule(sample_server,'sample',mapChoice)
-  
+
+    callModule(query_server,'query',tara_data,query_table)
+
+    mapOut =  callModule(map_server,'map', query_table)
+
+    callModule(sample_server,'sample',mapOut$outSample)
+
 })
