69f643575f15a895db9b55f9ee23bb661b2739ab
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-10-12 23:43:35 -0700

    experimental pre-processing

--- global.R
+++ global.R
@@ -13,4 +13,10 @@ for(module in modules){
 }
 
 
+
+# rds files are created at preprocessing.R
 tara_data = read_rds('tara_data.rds')
+# global_mean is pathway_means accross all samples
+# meant as a preliminiary comparison baseline
+# doesn't make too much sense as there are bacteria and viruses in this thing
+global_means = read_rds('global_means.rds')
--- preprocessing.R
+++ preprocessing.R
@@ -11,3 +11,7 @@ tara_data = read_tsv('df_MASTERTABLE.txt')
 tara_data %<>% mutate(virus = !grepl('^c',SAMPLE))
 
 write_rds(tara_data,'tara_data.rds')
+
+
+globalMeans = tara_data %>% group_by(PWY_NAME) %>% summarise(mean = mean(RPKM))
+write_rds(globalMeans,'global_means.rds')
