2b5a2251c6939dea7918861897b0847d3317e213
klgray25 <kristen.leanne252@gmail.com>
2018-10-14 15:06:37 -0700

    Minor edits

--- modules/future.R
+++ modules/future.R
@@ -3,6 +3,7 @@ future_ui = function(){
              HTML('<center><img src = "../tara_logo.png", height="240", width="240"></center>'),
              tags$h1("Future Directions"),
              HTML('<br><br>'),
-             tags$p('Viral Voyager is a website that shows a large-scale mapping of analysed metaviriome data collected from the Tara Oceans Project.')
+             tags$p('Viral Voyager is a website that shows a large-scale mapping of analysed metaviriome 
+                    data collected from the Tara Oceans Project.')
              
     )}
