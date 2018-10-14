83746d18a9cf6d1076c126d6bd75e36c7f51f570
Dan Fornika <dfornika@gmail.com>
2018-10-13 17:18:09 -0700

    dcast table

--- experimental_scripts/distance_matrix.R
+++ experimental_scripts/distance_matrix.R
@@ -1,7 +1,9 @@
+library(reshape2)
 
 tara_data = read_tsv('data/df_MASTERTABLE.txt')
 
 calculate_distance_matrix <- function (df) {
     samples <- df %>% select(SAMPLE, PWY_NAME, LEVEL1, LEVEL2, RPKM)
+    samples %<>% dcast(SAMPLE ~ PWY_NAME)
     samples
 }
