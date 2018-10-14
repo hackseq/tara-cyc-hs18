2c7ff0cd77155ba28d4c88c3efc865f75e6e84f3
klgray25 <kristen.leanne252@gmail.com>
2018-10-14 15:08:52 -0700

    Edited Future Directions

--- modules/future.R
+++ modules/future.R
@@ -3,7 +3,11 @@ future_ui = function(){
              HTML('<center><img src = "../tara_logo.png", height="240", width="240"></center>'),
              tags$h1("Future Directions"),
              HTML('<br><br>'),
-             tags$p('Viral Voyager is a website that shows a large-scale mapping of analysed metaviriome 
-                    data collected from the Tara Oceans Project.')
+             tags$p('In the future, Virus Voyager will update and integrate data sets and become the platform for global ocean biomes for both researchers and general audiences. These datasets can be found on MGnify’s website:
+https://www.ebi.ac.uk/metagenomics/studies/ERP001736
+                    https://www.ebi.ac.uk/metagenomics/api/v1/studies/MGYS00000410
+                    https://www.ebi.ac.uk/metagenomics/api/v1/analyses
+                    https://www.ebi.ac.uk/metagenomics/studies/MGYS00001482
+                    We will also explore more interactive and user friendly analytical functions utilizing the existing vast Tara ocean virus data set. We will look to complete a well documented manual script.')
              
     )}
